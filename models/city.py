4f7d85a2387f0f5a98fc37c688be2741f7572b21
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-11-29 14:16:12 +0000

    Revert relationship change

--- models/city.py
+++ models/city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
@@ -11,5 +10,3 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-
-    places = relationship("Place", backref="cities", cascade="delete")
