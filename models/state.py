3b1d47ffe7523b8242d1ab29632558bb1f003a41
JeremMac <macal.jerem@gmail.com>
2023-11-29 13:50:25 +0100

    Added modified city, state & base_model files.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
+Base = declarative_base
 class State(BaseModel):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            from models import storage
+            city_instances = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    city_instances.append(city)
+            return city_instances
