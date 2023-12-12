9b877771e22cfc3a140b56c7bacef88f983a6ab9
JeremMac <macal.jerem@gmail.com>
2023-12-12 13:46:54 +0100

    Modified dbstorage and filestorage.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,3 +86,6 @@ class DBStorage:
         # Thread-local session to avoid issues when multithreading
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        self.__session.remove()
