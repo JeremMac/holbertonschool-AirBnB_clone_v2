77b40b8a34af2bdb7b05fb6222034725996b2e99
JeremMac <macal.jerem@gmail.com>
2023-12-12 10:49:29 +0100

    Hightcase change.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,9 +38,9 @@ def is_int(n):
     return f'{n} is a number'
 
 
-@app.route(f"/number_template/<int:n>", strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def display_html(n):
-    return render_template("5-number.html", number=n)
+    return render_template("/5-number.html", n=n)
 
 
 if __name__ == '__main__':
